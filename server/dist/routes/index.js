8290574e22732cb27a2c2bb63a9f8e473ce0110d
M Possible <185700337+MTS-sip@users.noreply.github.com>
2025-03-09 08:11:09 -0400

    reset render

--- server/dist/routes/index.js
+++ server/dist/routes/index.js
@@ -1,17 +1,11 @@
 import { Router } from "express";
 import authRoutes from "./auth-routes.js";
 import authenticateToken from "../middleware/auth.js";
-
 const router = Router();
-
 // Authentication routes (Login)
 router.use("/auth", authRoutes);
-
 // Protected API routes
 router.use("/tickets", authenticateToken, (_req, res) => {
     res.json({ message: "Protected ticket data" });
 });
-
 export default router;
-
-
